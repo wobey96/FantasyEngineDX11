1f8871ba44e3f61c1a1e96d96494aa2765218cf4
wobey000 <wobey96@gmail.com>
2025-11-05 12:48:33 -0700

    "middle of creating windowing system"

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,8 +1,28 @@
 ﻿#include <iostream>
 
+#define GLFW_INCLUDE_NONE 
+#include <GLFW/glfw3.h>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3native.h>
+
 
 int main()
 {
-	std::cout << " Hello World \n"; 
+	if (!glfwInit())
+	{
+		abort(); 
+	}
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); 
+
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "FantasyEngineDX11", nullptr, nullptr); 
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents(); 
+	}
+
+	glfwTerminate(); 
 	return 0;
 }
