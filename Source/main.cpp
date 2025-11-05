813648152f4c3dcf755e10a3dad9e0c6d7275713
wobey000 <wobey96@gmail.com>
2025-11-05 09:23:09 -0700

    Add project files.

---
+++ Source/main.cpp
@@ -0,0 +1,12 @@
+﻿// FantasyEngineDX11.cpp : Defines the entry point for the application.
+//
+
+#pragma once
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
